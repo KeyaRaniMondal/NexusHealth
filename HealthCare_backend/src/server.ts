0e4bc4a58dd64fc52f4d9bd8dd29aaa66f06cda5
KeyaRaniMondal <keyamondal549@gmail.com>
2026-08-12 21:31:43 +0600

    feat: configured redis database

--- HealthCare_backend/src/server.ts
+++ HealthCare_backend/src/server.ts
@@ -1,18 +1,21 @@
 import app from "./app";
 import config from "./app/config";
 import { prisma } from "./app/lib/prisma";
+import { redisClient } from "./app/lib/redis";
 import {
 	seedSuperAdmin,
 	seedTesterAdmin,
 	seedTesterDoctor,
 } from "./app/utils/seed";
-
 const PORT = config.port;
 
 const main = async () => {
 	try {
 		await prisma.$connect();
 		console.log("Connected to the database successfully.");
+
+        await redisClient.connect();
+        console.log("Connected to Redis successfully.");
 
 		await seedSuperAdmin();
 		await seedTesterAdmin();
